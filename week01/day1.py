a161012f30d6fd06fa5393224ac8b7be4d2eee11
Anne Chepkeitany <rerimoianne.4@gmail.com>
2018-07-23 15:47:53 +0300

    Partial submission to week1 day1 code exercises

---
+++ week01/day1.py
@@ -0,0 +1,26 @@
+# Write a program that tells you the following:
+
+# Hours in a year. How many hours are in a year?
+# (Assuming a non-leap year)
+hours_in_a_year = 365 * 24
+print("There are " + str(hours_in_a_year) + " hours in a year.\n")
+
+
+# Minutes in a decade. How many minutes are in a decade?
+minutes_in_a_decade = 10 * 365 * 24 * 60
+print("There are " + str(minutes_in_a_decade) + " minutes in a decade.\n")
+
+
+# Your age in seconds. How many seconds old are you? (I'm not going to check your answer, so be as accurate—or not—as you want.)
+my_age_in_seconds = 25 * 365 * 24 * 60 * 60
+print("My age in seconds is " + str(my_age_in_seconds) + "\n")
+
+
+# Andreea Visanoiu​: I'm 48618000 seconds old hahaha. Calculate @Andreea Visanoiu's age.
+age = 48618000 / (365 * 24 * 60 * 60)
+print("Andreea's age is " + str(age) + " years\n")
+
+# How many days does it take for a 32-bit system to timeout, if it has a bug with integer overflow?
+# How about a 64-bit system?
+
+# Calculate your age accurately based on your birthday (maybe use time of day e.g. 8:23am if you know it, use 12:00 noon midday) - you will need Python modules.
